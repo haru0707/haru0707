2402874925f7b5368b69fbb47c8ba84a1b12a68f
haru0707 <95564227+haru0707@users.noreply.github.com>
2025-08-20 00:52:22 +0900

    Update README.c

--- README.c
+++ README.c
@@ -1,12 +1,13 @@
 - 👋 Hi, I’m @haru0707
 - 👀 I’m interested in LLM and OSS
 - 🌱 I’m currently learning JS
-- 📫 If you want to conntact  me,pls use discord. My ID is haru0707kun
+- 📫 You can reach me on Discord. My ID is haru0707kun.
 - I am Japanese.
 - I don't know much about programming. If you have any strange points or advice, I would appreciate it if you could let me know.
 This sentence uses Google Translate and may be strange. 
 - 日本語
 - こんにちは私はプログラミング初心者ですアドバイスなどがあれば教えてください
+- もし機会があればOSSプロジェクトに参加したいと考えています、声をかけていただけると助かります
 
 <!---
 haru0707/haru0707 is a ✨ special ✨ repository because its `README.md` (thpais file) appears on your GitHub profile.
